d1fcc9744d149bcc61636ed9177936498e4b332f
alejandro <alejosamp@gmail.com>
2024-05-01 17:21:48 -0500

    implemented security consifg and jwt services

---
+++ book-social-network/src/main/java/com/alejoskylosky/booksocialnetwork/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.alejoskylosky.booksocialnetwork.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alejoskylosky.booksocialnetwork.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository repository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
